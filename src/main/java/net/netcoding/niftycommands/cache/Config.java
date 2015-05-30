0b1de93800cb300c0e4dc056d9bb2ace95e1b296
Brian Graham <support@netcoding.net>
2015-05-30 01:12:03 -0400

    Added a method to generate velocity from pitch

--- src/main/java/net/netcoding/niftycommands/cache/Config.java
+++ src/main/java/net/netcoding/niftycommands/cache/Config.java
@@ -4,6 +4,7 @@ import net.netcoding.niftycore.database.MySQL;
 import net.netcoding.niftycore.yaml.SQLConfig;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class Config extends SQLConfig<MySQL> {
 
@@ -14,5 +15,26 @@ public class Config extends SQLConfig<MySQL> {
 	public Config(JavaPlugin plugin) {
 		super(plugin.getDataFolder(), "config");
 	}
+
+    public static Vector getPitchVelocity(float pitch, double velocity) {
+        Vector vector = new Vector();
+
+        if (pitch >= 0 && pitch <= 45.0)
+            vector.setZ(velocity);
+
+        if (pitch >= 45.0 && pitch <= 135.0)
+            vector.setX(-velocity);
+
+        if (pitch >= 135.0 && pitch <= 225.0)
+            vector.setZ(-velocity);
+
+        if (pitch >= 225.0 && pitch <= 315.0)
+            vector.setX(velocity);
+
+        if (pitch >= 315.0 && pitch <= 360.0)
+            vector.setZ(velocity);
+
+        return vector;
+    }
 
 }
